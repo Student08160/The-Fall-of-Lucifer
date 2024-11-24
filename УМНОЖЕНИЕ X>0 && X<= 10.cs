fa034b2e358061ec28eefb9d493a9db15c9cc1ff
Student08160 <Presentvsegda@yandex.ru>
2024-11-24 19:41:17 +0300

    Update УМНОЖЕНИЕ X>0 && X<= 10.cs

--- УМНОЖЕНИЕ X>0 && X<= 10.cs
+++ УМНОЖЕНИЕ X>0 && X<= 10.cs
@@ -1,40 +1,47 @@
-try
+static void Main(string[] args)
 {
-    // Объявляем необходимые переменные.
-    int a = 1, b = 1;
-    while (true)
+    try
     {
-        // Считываем исходные данные от пользователя.
-        Console.Write("Пожалуйста, введите первое число: ");
-        a = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("Пожалуйста, введите второе число: ");
-        b = Convert.ToInt32(Console.ReadLine());
-
-        // Делаем условие вычисления.
-        if ((a < 0  a > 10) && (b < 0  b > 10))
+        // Объявляем необходимые переменные.
+        int a = -1, b = -1;
+        while (true)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Введённые числа недопустимы.");
-            Console.ForegroundColor = ConsoleColor.White;
+            // Считываем исходные данные от пользователя.
+            Console.Write("Пожалуйста, введите первое число: ");
+            a = Convert.ToInt32(Console.ReadLine());
 
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Пожалуйста, попробуйте ещё раз.");
-            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Пожалуйста, введите второе число: ");
+            b = Convert.ToInt32(Console.ReadLine());
 
-            Main(args);
+            // Делаем условие вычисления.
+            if ((a >= 0 && a <= 10) && (b >= 0 && b <= 10))
+            {
+                // Производим вычисления.
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Результат: {a * b}");
+                Console.ForegroundColor = ConsoleColor.White;
+                break;
+            } 
+            else // ((a < 0 && a > 10) && (b < 0 && b > 10))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Введённые числа недопустимы.");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Пожалуйста, попробуйте ещё раз.");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                continue;
+
+            }
         }
-
-        // Производим вычисления.
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine($"Результат: {a * b}");
-        Console.ForegroundColor = ConsoleColor.White;
     }
-}
-catch (System.FormatException)
-{
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine("Неверный формат данных.");
-    Console.ForegroundColor = ConsoleColor.White;
-    Main(args);
+    catch (System.FormatException)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Неверный формат данных.");
+        Console.ForegroundColor = ConsoleColor.White;
+        Main(args);
+    }
 }
